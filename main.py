9967b6d0bc2e6c5ee76e726521dd04cce0de7200
unknown <projectwilsen@gmail.com>
2024-04-29 09:52:55 +0700

    add

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from neon_connector.neon_connector import NeonConnector
 from dotenv import load_dotenv
 import os 
+import json
 
 load_dotenv()
 connection_string = os.getenv('DATABASE_URL')
@@ -46,31 +47,82 @@ set_identity('Ger Sen wilsenp@gmail.com')
 # final_df['updated_on'] = pd.Timestamp.now()
 # final_df.to_csv('institutional_profile.csv', index=False)
 
-institution_profle_data = nc.select_query("SELECT cik FROM institution_profile")
-all_cik = [entry['cik'] for entry in institution_profle_data]
-print(all_cik)
+# INSTITUTION FILING
 
-consecutive_failures = 0
-for cik in all_cik[6500:7500]:
-    filing_data = []
-    for i in range(len(find(cik).get_filings(form="13F-HR"))):
-        try:
-            data = find(cik).get_filings(form="13F-HR")[i].obj()
-            filing_data.append({"cik": cik,
-                                "accession_number" :data.filing.accession_no,
-                                "report_period" :data.primary_form_information.report_period,
-                                "filing_date": data.filing.filing_date,
-                                "total_value": int(data.primary_form_information.summary_page.total_value),
-                                "total_holding": int(data.primary_form_information.summary_page.total_holdings)
-                                })
-            print(f"success {cik} {i}")
-            consecutive_failures = 0
-        except:
-            print(f"fail {cik} {i}")
-            consecutive_failures += 1
-            if consecutive_failures > 2:
-                print(f"Skipping CIK: {cik}")
-                break 
+# institution_profle_data = nc.select_query("SELECT cik FROM institution_profile")
+# all_cik = [entry['cik'] for entry in institution_profle_data]
+# print(all_cik)
 
-    nc.batch_upsert(target_table="form_13f_filing", records=filing_data, conflict_columns=['accession_number'])
-    print(f"====== {cik} is inserted ======")
+# consecutive_failures = 0
+# for cik in all_cik[6500:7500]:
+#     filing_data = []
+#     for i in range(len(find(cik).get_filings(form="13F-HR"))):
+#         try:
+#             data = find(cik).get_filings(form="13F-HR")[i].obj()
+#             filing_data.append({"cik": cik,
+#                                 "accession_number" :data.filing.accession_no,
+#                                 "report_period" :data.primary_form_information.report_period,
+#                                 "filing_date": data.filing.filing_date,
+#                                 "total_value": int(data.primary_form_information.summary_page.total_value),
+#                                 "total_holding": int(data.primary_form_information.summary_page.total_holdings)
+#                                 })
+#             print(f"success {cik} {i}")
+#             consecutive_failures = 0
+#         except:
+#             print(f"fail {cik} {i}")
+#             consecutive_failures += 1
+#             if consecutive_failures > 2:
+#                 print(f"Skipping CIK: {cik}")
+#                 break 
+
+#     nc.batch_upsert(target_table="form_13f_filing", records=filing_data, conflict_columns=['accession_number'])
+#     print(f"====== {cik} is inserted ======")
+
+
+# INSTITUTION HOLDING
+
+sql_query = """
+WITH ranked_filings AS (
+    SELECT 
+        filing_id, 
+        cik, 
+        filing_date, 
+        accession_number,
+        ROW_NUMBER() OVER (PARTITION BY cik ORDER BY filing_date DESC) AS rank_num
+    FROM 
+        form_13f_filing
+)
+SELECT 
+    filing_id, 
+    cik, 
+    filing_date, 
+    accession_number
+FROM 
+    ranked_filings
+WHERE 
+    rank_num <= 2
+"""
+
+latest_filings_list = nc.select_query(sql_query)
+print(latest_filings_list)
+
+for filing in latest_filings_list[10:]:
+    df = find(filing['accession_number']).obj().infotable.sort_values('Value', ascending = False)[['Ticker','Value','SharesPrnAmount']]
+    df['filing_id'] = filing['filing_id']
+    df[['Value','SharesPrnAmount']] = df[['Value','SharesPrnAmount']].astype(int)
+    df = df.groupby(['Ticker']).sum().reset_index().sort_values('Value', ascending = False)
+    total_shares = df['Value'].sum()
+    df['percentage'] = df['Value'] / total_shares * 100
+    df = df.rename(columns={'Ticker': 'symbol', 'Value': 'value','SharesPrnAmount':'share'})
+    df['filing_date'] = filing['filing_date'] 
+    json_string = df.to_json(orient='records')
+    recs = json.loads(json_string)
+    try:
+        nc.batch_upsert(target_table="form_13f_holdings", records=recs, conflict_columns=['filing_id','symbol'])
+    except:
+        df['filing_date'] = df['filing_date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
+        json_string = df.to_json(orient='records')
+        recs = json.loads(json_string)
+        nc.batch_upsert(target_table="form_13f_holdings", records=recs, conflict_columns=['filing_id','symbol'])
+    print(recs)
+
